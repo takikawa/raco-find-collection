7d87b531568e08d48f3e99c8c0d65ef91db518b2
Asumu Takikawa <asumu@ccs.neu.edu>
2013-10-19 18:45:14 -0400

    Fix error case with 'relative split-path results

--- find-collection/find-collection.rkt
+++ find-collection/find-collection.rkt
@@ -9,10 +9,16 @@
             (define-values (base name _1)
               (split-path collection))
             (define file-path
-              (collection-file-path name base #:fail (λ (_) #f)))
+              (and (path-string? base)
+                   (path-string? name)
+                   (collection-file-path name base #:fail (λ (_) #f))))
             (and file-path
                  (file-exists? file-path)
                  (let-values ([(collection-base _2 _3)
                                (split-path (collection-file-path name base))])
                    collection-base)))))
+
+(module+ test
+  (require rackunit)
+  (check-not-exn (λ () (find-collection-dir "raco-find-collection"))))
 
