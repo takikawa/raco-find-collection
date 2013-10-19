7b30d87d1242583a86718da519cd44a9ed882c15
Asumu Takikawa <asumu@ccs.neu.edu>
2013-10-19 18:49:30 -0400

    Don't raise an exception early on non-relative path

--- find-collection/find-collection.rkt
+++ find-collection/find-collection.rkt
@@ -2,23 +2,30 @@
 
 (provide find-collection-dir)
 
+;; Path-String -> (Option Path)
 (define (find-collection-dir collection)
-  (collection-path
-   collection
-   #:fail (λ (_0)
-            (define-values (base name _1)
-              (split-path collection))
-            (define file-path
-              (and (path-string? base)
-                   (path-string? name)
-                   (collection-file-path name base #:fail (λ (_) #f))))
-            (and file-path
-                 (file-exists? file-path)
-                 (let-values ([(collection-base _2 _3)
-                               (split-path (collection-file-path name base))])
-                   collection-base)))))
+  (and (relative-path? collection)
+       (collection-path
+        collection
+        #:fail (file-path-case collection))))
+
+;; Path-String -> Any -> (Option Path)
+;; Dummy argument to satisfy `collection-path` contract
+(define ((file-path-case collection) _0)
+  (define-values (base name _1)
+    (split-path collection))
+  (define file-path
+    (and (path-string? base)
+         (path-string? name)
+         (collection-file-path name base #:fail (λ (_) #f))))
+  (and file-path
+       (file-exists? file-path)
+       (let-values ([(collection-base _2 _3)
+                     (split-path (collection-file-path name base))])
+         collection-base)))
 
 (module+ test
   (require rackunit)
+  (check-not-exn (λ () (find-collection-dir "/bin/bash")))
   (check-not-exn (λ () (find-collection-dir "raco-find-collection"))))
 
