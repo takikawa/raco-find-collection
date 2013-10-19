af097651f9a107d627ea44e99e7a0d872e7926ff
Asumu Takikawa <asumu@ccs.neu.edu>
2013-10-19 18:25:50 -0400

    Also print an error if the collection wasn't found

--- find-collection/run.rkt
+++ find-collection/run.rkt
@@ -6,7 +6,10 @@
 (command-line
  #:args (collection-path)
  (let ([dir (find-collection-dir collection-path)])
-   (if dir
-       (displayln (path->string dir))
-       (displayln (path->string (current-directory))))))
+   (cond [dir (displayln (path->string dir))]
+         [else
+          (displayln (path->string (current-directory)))
+          (error 'raco-find-collection
+                 "could not find the collection path ~v"
+                 collection-path)])))
 
