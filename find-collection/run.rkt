99b6e18ca4b26512618f344767db1277b0abfc7a
Asumu Takikawa <asumu@ccs.neu.edu>
2013-10-28 16:48:55 -0400

    Improve error handling
    Make more error cases print the current directory so
    that the shell script doesn't `cd` somewhere else

--- find-collection/run.rkt
+++ find-collection/run.rkt
@@ -3,13 +3,19 @@
 (require "find-collection.rkt"
          racket/cmdline)
 
-(command-line
- #:args (collection-path)
- (let ([dir (find-collection-dir collection-path)])
-   (cond [dir (displayln (path->string dir))]
-         [else
-          (displayln (path->string (current-directory)))
-          (raise-user-error 'raco-find-collection
-                            "could not find the collection path ~v"
-                            collection-path)])))
+;; print current directory (for use in scripts) and
+;; re-raise the exception to print it to stderr
+(define (fail e)
+  (displayln (path->string (current-directory)))
+  (raise e))
+
+(with-handlers ([exn:fail? fail])
+  (command-line
+   #:args (collection-path)
+   (let ([dir (find-collection-dir collection-path)])
+     (cond [dir (displayln (path->string dir))]
+           [else
+            (raise-user-error 'raco-find-collection
+                              "could not find the collection path ~v"
+                              collection-path)]))))
 
